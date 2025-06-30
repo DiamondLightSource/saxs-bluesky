a7fef41342b47fec5aacd54af4cf5c3c8763cce4
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-06-30 13:43:36 +0000

    add new ixx_profiles

---
+++ src/sas_bluesky/beamline_configs/b21_profiles.py
@@ -0,0 +1,20 @@
+from sas_bluesky.profile_groups import Group, Profile  # ignore
+
+DEFAULT_GROUP = Group(
+    frames=1,
+    wait_time=1,
+    wait_units="S",
+    run_time=1,
+    run_units="S",
+    pause_trigger="IMMEDIATE",
+    wait_pulses=[1, 0, 0, 0, 0, 0],
+    run_pulses=[1, 1, 0, 0, 0, 0],
+)
+
+
+DEFAULT_PROFILE = Profile(
+    cycles=1,
+    seq_trigger="IMMEDIATE",
+    groups=[DEFAULT_GROUP],
+    multiplier=[1, 1, 1, 1, 1, 1],
+)
