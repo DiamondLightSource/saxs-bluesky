19157948cc54c54854935edd6a7da1d9dc3e4875
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-08-27 15:35:42 +0000

    changed sets to lists, set non serialisable

--- src/saxs_bluesky/defaults_configs/b21/b21_dev.py
+++ src/saxs_bluesky/defaults_configs/b21/b21_dev.py
@@ -2,5 +2,5 @@ from dodal.common import inject
 from ophyd_async.core import StandardDetector
 from ophyd_async.fastcs.panda import HDFPanda
 
-FAST_DETECTORS: set[StandardDetector] = {inject("saxs"), inject("waxs")}
+FAST_DETECTORS: list[StandardDetector] = [inject("saxs"), inject("waxs")]
 DEFAULT_PANDA: HDFPanda = inject("panda2")
