0d54b32b9ef36e61b76ce7d18778ab53d73631cc
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-09-12 10:14:34 +0000

    moved baseline to run_triggering

--- src/saxs_bluesky/plans/ncd_panda.py
+++ src/saxs_bluesky/plans/ncd_panda.py
@@ -411,6 +411,7 @@ def configure_panda_triggering(
 
 
 @attach_data_session_metadata_decorator()
+@bpp.baseline_decorator(DEFAULT_BASELINE)
 @bpp.run_decorator()  #    # open/close run
 @validate_call(config={"arbitrary_types_allowed": True})
 def run_panda_triggering(
@@ -476,7 +477,6 @@ def run_panda_triggering(
 
 
 @bpp.run_decorator()  #    # open/close run
-@bpp.baseline_decorator(DEFAULT_BASELINE)
 @attach_data_session_metadata_decorator()
 def configure_and_run_panda_triggering(
     profile: Annotated[
