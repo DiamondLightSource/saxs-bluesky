3b6ec0c47efb9dd61d5f771f548b1e94478d2af0
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-09-16 11:50:13 +0000

    added test for pandagui

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,6 +1,15 @@
-# from saxs_bluesky.panda_gui import PandAGUI
-# from saxs_bluesky.panda_gui_elements import EditableTableview
+from saxs_bluesky.gui.panda_gui import PandAGUI
+from saxs_bluesky.utils.utils import (
+    get_saxs_beamline,
+    load_beamline_config,
+)
+
+############################################################################################
+
+BL = get_saxs_beamline()
+CONFIG = load_beamline_config()
 
 
 def test_panda_gui():
-    pass
+    CONFIG = load_beamline_config()
+    PandAGUI(configuration=CONFIG.DEFAULT_EXPERIMENT, start=False)
