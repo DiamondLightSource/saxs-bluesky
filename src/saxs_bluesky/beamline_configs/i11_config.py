a60f2b078b937ad360579da20e3ea3b3b5a865c9
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-09-26 14:30:13 +0000

    add i11 and p38

---
+++ src/saxs_bluesky/beamline_configs/i11_config.py
@@ -0,0 +1,125 @@
+from copy import deepcopy
+
+from bluesky.protocols import Readable
+from dodal.beamlines import i11
+from dodal.common import inject
+from ophyd_async.core import StandardDetector
+from ophyd_async.fastcs.panda import HDFPanda
+
+from saxs_bluesky.utils.beamline_client import BlueAPIPythonClient
+from saxs_bluesky.utils.profile_groups import ExperimentLoader, Group, Profile
+
+"""
+
+Configuration for b21 PandA beamline
+
+"""
+
+DEFAULT_INSTRUMENT_SESSION = "cm40625-4"
+
+###THESE NEED TO BE LISTS TO BE SERIALISED
+FAST_DETECTORS: list[StandardDetector] = [inject("mythen")]
+
+DEFAULT_PANDA: HDFPanda = inject("panda1")
+
+DEFAULT_BASELINE: list[Readable] = [
+    inject("slits_1"),
+    inject("slits_2"),
+    inject("slits_3"),
+    inject("slits_5"),
+    inject("slits_6"),
+    inject("synchrotron"),
+]
+
+
+# GUI Elements
+PULSEBLOCKS = 4  # this is higher than the number of pulseblocks
+# so each connection cant have a pulseblock for mutpliers
+PULSEBLOCKASENTRYBOX = False
+PULSE_BLOCK_NAMES = ["mythen", "None", "None", "None"]
+THEME_NAME = "clam"  # --> ('clam', 'alt', 'default', 'classic')
+
+# PandA Wiring connections
+
+TTLIN = {1: "Beamstop V2F", 2: None, 3: None, 4: "TFG WAXS", 5: "TFG FS", 6: "TFG SAXS"}
+
+TTLOUT = {
+    1: "mythen",
+    2: None,
+    3: None,
+    4: None,
+    5: None,
+    6: None,
+    7: None,
+    8: None,
+    9: None,
+    10: None,
+}
+
+
+LVDSIN = {1: None, 2: None}
+
+
+LVDSOUT = {1: None, 2: None}
+
+PULSE_CONNECTIONS = {
+    1: [TTLOUT[1]],
+    2: [TTLOUT[2], TTLOUT[3]],
+    3: [TTLOUT[4]],
+    4: [TTLOUT[5]],
+    5: [TTLOUT[6]],
+    6: [TTLOUT[7]],
+}
+
+
+"""
+
+# Panda plan parameters
+
+"""
+
+# Buffer added to deadtime to handle minor discrepencies between detector
+# and panda clocks
+DEADTIME_BUFFER = 20e-6
+# default sequencer is this one, b21 currently uses seq 1 for somthing else
+DEFAULT_SEQ = 2
+
+CONFIG_NAME = "PandaTrigge"
+
+
+"""
+
+# DEFAULT PROFILES
+
+"""
+
+DEFAULT_GROUP = Group(
+    frames=1,
+    trigger="IMMEDIATE",
+    wait_time=1,
+    wait_units="S",
+    run_time=1,
+    run_units="S",
+    wait_pulses=[1, 0, 0, 0],
+    run_pulses=[1, 1, 0, 0],
+)
+
+
+DEFAULT_PROFILE = Profile(
+    repeats=1,
+    seq_trigger="IMMEDIATE",
+    groups=[deepcopy(DEFAULT_GROUP)],
+    multiplier=[1, 1, 1, 1],
+)
+
+DEFAULT_EXPERIMENT = ExperimentLoader(
+    profiles=[deepcopy(DEFAULT_PROFILE)],
+    instrument=i11.BL,
+    detectors=FAST_DETECTORS,
+    instrument_session=DEFAULT_INSTRUMENT_SESSION,
+)
+
+
+# BlueAPI client
+blueapi_config_path = f"./src/saxs_bluesky/blueapi_configs/{i11.BL}_blueapi_config.yaml"
+CLIENT = BlueAPIPythonClient(i11.BL, blueapi_config_path, DEFAULT_INSTRUMENT_SESSION)
---
+++ src/saxs_bluesky/beamline_configs/p38_config.py
@@ -0,0 +1,112 @@
+"""
+
+Configuration for i22 PandA beamline
+
+"""
+
+from copy import deepcopy
+
+from bluesky.protocols import Readable
+from dodal.beamlines import p38
+from dodal.common import inject
+from ophyd_async.core import StandardDetector
+from ophyd_async.fastcs.panda import HDFPanda
+
+from saxs_bluesky.utils.beamline_client import BlueAPIPythonClient
+from saxs_bluesky.utils.profile_groups import ExperimentLoader, Group, Profile
+
+DEFAULT_INSTRUMENT_SESSION = "cm40643-4"
+
+###THESE NEED TO BE LISTS TO BE SERIALISED
+
+FAST_DETECTORS: list[StandardDetector] = [
+    inject("saxs"),
+    inject("waxs"),
+]
+
+DEFAULT_PANDA: HDFPanda = inject("panda1")
+
+DEFAULT_BASELINE: list[Readable] = []
+
+# GUI Elements
+PULSEBLOCKS = 4
+PULSEBLOCKASENTRYBOX = False
+PULSE_BLOCK_NAMES = ["FS", "DETS/TETS", "OAV", "Fluro"]
+THEME_NAME = "clam"  # --> ('clam', 'alt', 'default', 'classic')
+
+# PandA Wiring connections
+
+TTLIN = {1: "", 2: None, 3: None, 4: "", 5: "", 6: ""}
+
+TTLOUT = {
+    1: "saxs",
+    2: "waxs",
+    3: None,
+    4: None,
+    5: None,
+    6: None,
+    7: None,
+    8: None,
+    9: None,
+    10: None,
+}
+
+
+LVDSIN = {1: None, 2: None}
+
+
+LVDSOUT = {1: None, 2: None}
+
+PULSE_CONNECTIONS = {
+    1: [TTLOUT[1]],
+    2: [TTLOUT[2], TTLOUT[3]],
+    3: [TTLOUT[4]],
+    4: [TTLOUT[5]],
+    5: [TTLOUT[6]],
+    6: [TTLOUT[7]],
+}
+
+
+"""
+# ncd plan parameters
+"""
+
+DEADTIME_BUFFER = 20e-6  # Buffer added to deadtime to handle minor discrepencies between detector and panda clocks #noqa
+DEFAULT_SEQ = 1  # default sequencer is this one, pandas can have 2
+CONFIG_NAME = "PandaTrigger"
+
+
+"""
+# DEFAULT PROFILES
+"""
+
+DEFAULT_GROUP = Group(
+    frames=1,
+    trigger="IMMEDIATE",
+    wait_time=10,
+    wait_units="MS",
+    run_time=100,
+    run_units="MS",
+    wait_pulses=[0, 0, 0, 0],
+    run_pulses=[1, 1, 1, 1],
+)
+
+
+DEFAULT_PROFILE = Profile(
+    repeats=1,
+    seq_trigger="IMMEDIATE",
+    groups=[deepcopy(DEFAULT_GROUP)],
+    multiplier=[1, 1, 1, 1],
+)
+
+DEFAULT_EXPERIMENT = ExperimentLoader(
+    profiles=[deepcopy(DEFAULT_PROFILE)],
+    instrument=p38.BL,
+    detectors=FAST_DETECTORS,
+    instrument_session=DEFAULT_INSTRUMENT_SESSION,
+)
+
+
+# BlueAPI client
+blueapi_config_path = f"./src/saxs_bluesky/blueapi_configs/{p38.BL}_blueapi_config.yaml"
+CLIENT = BlueAPIPythonClient(p38.BL, blueapi_config_path, DEFAULT_INSTRUMENT_SESSION)
