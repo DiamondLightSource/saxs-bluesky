0d315545ae47ad7dba870b617f1d781071d01f7f
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-09-26 14:31:11 +0000

    add i11 and p38 tests

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -1,4 +1,4 @@
-from saxs_bluesky.beamline_configs import b21_config, i22_config
+from saxs_bluesky.beamline_configs import b21_config, i11_config, i22_config, p38_config
 
 
 def test_i22_profile():
@@ -7,3 +7,11 @@ def test_i22_profile():
 
 def test_b21_profile():
     assert len(b21_config.DEFAULT_PROFILE.groups[0].run_pulses) == 6
+
+
+def test_i11_profile():
+    assert len(i11_config.DEFAULT_PROFILE.groups[0].run_pulses) == 4
+
+
+def test_p38_profile():
+    assert len(p38_config.DEFAULT_PROFILE.groups[0].run_pulses) == 4
