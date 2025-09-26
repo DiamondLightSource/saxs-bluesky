c3b8fd243c80de02b0cea3086c16ee312e8be831
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-09-26 12:10:42 +0000

    remove gui tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,6 +1,3 @@
-import os
-
-from saxs_bluesky.gui.panda_gui import PandAGUI
 from saxs_bluesky.utils.utils import (
     get_saxs_beamline,
     load_beamline_config,
@@ -11,11 +8,6 @@ from saxs_bluesky.utils.utils import (
 BL = get_saxs_beamline()
 CONFIG = load_beamline_config()
 
-if os.environ.get("DISPLAY", "") == "":
-    os.environ.__setitem__("DISPLAY", ":0.0")
-    os.system("export DISPLAY=:0.0")
-    os.system("Xvfb :0.0 &")
-
 
 def test_panda_gui():
-    PandAGUI(configuration=CONFIG.DEFAULT_EXPERIMENT)
+    pass
