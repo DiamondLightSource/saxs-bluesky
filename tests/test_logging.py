652b0666cff2b11cd5dd1f63b57730002a5c0b31
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-10-29 09:10:03 +0000

    add messenger creation test

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -2,7 +2,11 @@ import json
 import time
 from collections import deque
 
-from saxs_bluesky.logging.bluesky_messenger import MessageUnpacker, ScanListener
+from saxs_bluesky.logging.bluesky_messenger import (
+    MessageUnpacker,
+    RabbitMQMessenger,
+    ScanListener,
+)
 
 
 def test_message_unpacker():
@@ -34,3 +38,19 @@ def test_scan_messenger():
     listener.on_error(MockMessage("error message 1"))
 
     assert len(listener.messages) == 1
+
+
+def test_messenger_creation():
+    messenger = RabbitMQMessenger(
+        host="localhost",
+        beamline="test_beamline",
+        port=61613,
+        username="user",
+        password="pass",
+        destination="/queue/test",
+        auto_connect=False,
+    )
+
+    assert messenger.host == "localhost"
+    assert messenger.port == 61613
+    assert messenger.beamline == "test_beamline"
