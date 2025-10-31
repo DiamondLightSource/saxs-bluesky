136e83447ef128e881f2e5087b601f0ab77dd33b
RJCD-Diamond <Richard.Dixey@diamond.ac.uk>
2025-10-31 17:49:22 +0000

    removed verbose error messages

--- src/saxs_bluesky/utils/beamline_client.py
+++ src/saxs_bluesky/utils/beamline_client.py
@@ -1,24 +1,9 @@
 from collections.abc import Callable
 from pathlib import Path
 
-from blueapi._version import __version__
 from blueapi.cli.updates import CliEventRenderer
 from blueapi.client.client import BlueapiClient
-from blueapi.client.event_bus import AnyEvent, BlueskyStreamingError
-from blueapi.client.rest import BlueskyRemoteControlError
-from packaging.version import Version
-
-if Version(__version__) > Version("1.6.1"):
-    from blueapi.client.rest import (
-        InvalidParametersError,
-        UnauthorisedAccessError,
-        UnknownPlanError,
-    )
-else:
-    from blueapi.client.rest import InvalidParameters as InvalidParametersError
-    from blueapi.client.rest import UnauthorisedAccess as UnauthorisedAccessError
-    from blueapi.client.rest import UnknownPlan as UnknownPlanError
-
+from blueapi.client.event_bus import AnyEvent
 from blueapi.config import (
     ApplicationConfig,
     ConfigLoader,
@@ -94,16 +79,8 @@ class BlueAPIPythonClient(BlueapiClient):
 
                 return
 
-            except UnknownPlanError as up:
-                raise Exception(f"Plan '{plan_name}' was not recognised: {up}") from up
-            except UnauthorisedAccessError as ua:
-                raise Exception(f"Unauthorised request {ua}") from ua
-            except InvalidParametersError as ip:
-                raise Exception(f"{ip.message()}: {ip}") from ip
-            except (BlueskyRemoteControlError, BlueskyStreamingError) as e:
-                raise Exception(f"server error with this message: {e}") from e
-            except ValueError as ve:
-                raise Exception(f"task could not run: {ve}") from ve
+            except Exception as e:
+                raise Exception(f"Task could not run: {e}") from e
 
         else:
             server_task = self.create_and_start_task(task)
