524b73d42ecf22cc6765b3d148bf8cc32b094169
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-11-04 12:50:03 +0000

    remove main test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,19 +1,9 @@
 import subprocess
 import sys
 
-import saxs_bluesky.__main__
 from saxs_bluesky import __version__
 
 
 def test_cli_version():
     cmd = [sys.executable, "-m", "saxs_bluesky", "--version"]
     assert subprocess.check_output(cmd).decode().strip() == __version__
-
-
-def test_main():
-    filepath = (saxs_bluesky.__main__).__file__
-
-    with open(filepath) as f:
-        code = f.read()
-
-    exec(code)
