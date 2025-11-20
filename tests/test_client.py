414d2e5743bd9ac49cc9226ff915d12c102a0cb4
root <root@localhost.localdomain>
2025-11-20 00:45:42 +0000

    tests working

--- tests/test_client.py
+++ tests/test_client.py
@@ -99,9 +99,43 @@ def test_blueapi_python_client_without_callback_run(
         client_without_callback.run(configure_panda_triggering)
 
 
+class Mockcode:
+    def __init__(self, name: str):
+        self.co_varnames = ("arg1", "arg2", "arg3")
+
+
 class MockPlanName:
     def __init__(self, name: str):
         self.__name__ = name
+        self.__code__ = Mockcode(name)
+
+
+@pytest.mark.parametrize(
+    "plan, args, kwargs",
+    (
+        ["plan", (), {}],
+        [MockPlanName("plan"), (1, 2, 3), {}],
+        [MockPlanName("plan"), (1, 2, 3), {"a": 1}],
+    ),
+)
+def test_run_with_valid_paraneters(
+    client: BlueAPIPythonClient, plan, args: tuple, kwargs: dict
+):
+    # Patch instance methods so run executes but no re calls happen.
+    with (
+        patch.object(client, "run_task", return_value=Mock()),
+        patch.object(
+            client, "create_and_start_task", return_value=Mock(task_id="t-fake")
+        ),
+        patch.object(client, "create_task", return_value=Mock(task_id="t-fake")),
+        patch.object(client, "start_task", return_value=Mock(task_id="t-fake")),
+    ):
+        assert client._events is not None
+        # Ensure the mocked event client can be used as a context manager if run uses it
+        client._events.__enter__ = Mock(return_value=client._events)
+        client._events.__exit__ = Mock(return_value=None)
+
+        client.run(plan, *args, **kwargs)
 
 
 @pytest.mark.parametrize(
